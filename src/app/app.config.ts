d07577bb0e9254ebcdb5eb1c206c0c17145d9757
SebasBazauri <sebasbazauri2003@gmail.com>
2024-07-12 16:49:20 -0500

    routing config commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SalaryCalculationComponent } from '../app/components/salary-calculation/salary-calculation.component'; 
+const routes: Routes = [
+  { path: '', component: SalaryCalculationComponent }, 
+];
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SalaryCalculationComponent } from '../app/components/salary-calculation/salary-calculation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SalaryCalculationComponent } // Ruta principal
+];
+
